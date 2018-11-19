7cd75cf7af9311038a3845d8fba40b8ebc8ff4d5
cstenico <k.stenico@gmail.com>
2018-11-19 10:44:08 -0200

    i want to play a game

--- JKP_dp.py
+++ JKP_dp.py
@@ -1,10 +1,17 @@
+#Camila 8530952
+#Henrique 8937225
+
 import random
 from collections import OrderedDict
 
-LineNbr=[-1, 1, 0, 0]
-ColNbr=[0, 0, -1, +1]
+#globals, don't touch (couldnt find anything as #define)
+EMPTY = 0
+ROCK = 1
+SCISSORS = 2
+PAPER = 3
 
-
+posVizinhoLinha=[-1, 1, 0, 0]
+posVizinhoColuna=[0, 0, -1, +1]
 
 class Game:
     def __init__(self, rows, columns):
@@ -14,11 +21,11 @@ class Game:
         self.memo=OrderedDict()
         self.board=[[0 for x in range(columns)] for y in range(rows)]
         self.solutions=[]
-        self.nSolutions=0
+        self.nsolutions=0
 
 
         #Zobrist hash table
-        #colocar o link do youtube
+        #https://www.youtube.com/watch?v=QYNRvMolN20
         self.hashTable = [[[random.randint(1, 2**64-1)for i in range(4)]
                        for j in range(columns)]
                       for k in range(rows)]
@@ -33,7 +40,8 @@ class Game:
     def printGame(self):
         for i in range(self.rows):
             for j in range(self.columns):
-                    print (self.board[i][j], end=' ')
+                print (self.board[i][j])
+                print (' ')
             print()
 
     """def Dynammic_Solver(self):
@@ -70,9 +78,77 @@ class Game:
                     islands+=1
                 if(islands > 1):
                     return False
+        return True    
+
+    def checkWin(self):
+        if (self.pieces == 1):
+            return True
+        elif :
+            return False
+
+    def calculateKey(self):
+        key = 0
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] != 0:
+                    key ^= self.hashtable[i][j][self.board[i][j]]
+        return key
+
+    def play (self):
+        success = False
+        state_hits = 0
+
+        self.rows = len(self.board)
+        self.columns = len(self.board[0])
+
+        key = computeKey(table, self.board)
+        if key in self.memo:
+            return self.memo[key]
+
+        if (not checkIslands(self.board)):
+            self.memo[key] = [False, 0]
+            return self.memo[key]
+            
+        if (checkWin(self)):
+            for i in range(len(self.board)):
+                for j in range(len(self.board[i])):
+                    if (self.board[i][j] != 0):
+                        self.solutions.append([i+1, j+1, self.board[i][j]])
+                        self.memo[key] = [True, 1]
+                        return self.memo[key]
+
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                current = self.board[i][j]
+                if (current != 0):
+                    for k in range(4): 
+                        iNbr = i + posVizinhoLinha[k]
+                        jNbr = j + posVizinhoColuna[k]
+                        if ((iNbr >= 0 and iNbr < self.rows) and (jNbr >= 0 and jNbr < self.columns)):
+                            neighbor = self.board[iNbr][jNbr]
+                            if (neighbor not in invalid_moves[current]):
+                                self.board[iNbr][jNbr] = current
+                                self.board[i][j] = 0
+                                self.pieces -= 1
+                                jump = solve(self.memo, table, self.board, self.solutions, self.pieces)
+                                if (jump[0]):
+                                    success = True
+                                    state_hits += jump[1]
+                                self.board[iNbr][jNbr] = neighbor
+                                self.board[i][j] = current
+                                self.pieces += 1
+        if (not success):
+            self.memo[key] = [False, 0]
+        else:
+            self.memo[key] = [True, state_hits]
+        return self.memo[key]
+
+
+def checkJanken (x1, x2):
+    if ((x1!=EMPTY and x2!=EMPTY) and ((x1==SCISSORS and x2==PAPER) or (x1==PAPER and x2==ROCK) or (x1==ROCK and x2==SCISSORS)))
         return True
-
-
+    elif 
+        return False
 
 
 def main():
@@ -80,8 +156,18 @@ def main():
     game=Game(int(sR), int(sC))
     game.readGame()
     game.printGame()
+    
+    # search for solutions
+    play(game)
+    hits = game.memo.popitem()[1][1]
 
-
+    print(hits)
+    print(len(game.solutions))
+    # display sorted unique solutions
+    for solution in sorted(game.solutions):
+        for value in solution:
+            print(str(value), end=' ')
+        print()
 
 if __name__ == '__main__':
     main()
